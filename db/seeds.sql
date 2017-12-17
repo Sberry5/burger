5b8d585ee64f9a169f28ba8c8df95ab256e0f790
Shannon Berry-Heller <shannonberry-heller@Shannons-MacBook.local>
2017-12-17 14:16:33 -0600

    All folders/files created

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Bacon Burger", TRUE);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Veggie Burger", FALSE);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Llama Burger", FALSE);
